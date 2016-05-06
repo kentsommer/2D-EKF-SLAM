ae6516b8ccc98e5a4aacde8a85cb4bf722cbe9f9
Joel <amert004@umn.edu>
2016-05-06 17:16:48 -0500

    updated plotting for known features and it saves video better, and changed wander and the kf propagate function to save known features but have not had a chance to test it

--- Plotting/RealTimePlotting.m
+++ Plotting/RealTimePlotting.m
@@ -10,24 +10,26 @@ A=dlmread('odomRun.txt');
 clear all
 close all
 
-savevideo=0;
+savevideo=1;
 
 Odomrun=[];
 Odomrunbytesize=0;
 Odomrunlength=0;
 
 Scan=[];
+NewScanPlot=plot(0,0,'MarkerSize', 0.00001);
 Scanbytesize=0;
 Scanlength=0;
 
 Features=[];
 Featuresbytesize=0;
 Featureslength=0;
-if savevideo
-V=VideoWriter('video.avi');
-V.FrameRate=10;
-open(V);
-end
+
+knownFeatures=[];
+knownFeaturesbytesize=0;
+knownFeatureslength=0;
+knownFeaturesPlot=plot(0,0,'MarkerSize', 0.00001);
+
 
 Pxy=[];
 Pxybytesize=0;
@@ -35,11 +37,22 @@ Pxylength=0;
 Ellipseplot=plot(0,0,'MarkerSize', 0.00001);
 currXY=[0;0];
 
-figure(1)
+fighandle=figure(1);
 hold on;
+dt=.1;
+
+if savevideo
+V=VideoWriter('video.avi');
+V.FrameRate=1/dt;
+open(V);
+fighandle.Resize='off';
+fighandle.Position=[0 0 1920 1080];
+end
+
+
 while(1)
     Odomrunfinfo=dir('../data/odom/odomRun.txt');
-    if ~isempty(Odomrunfinfo.bytes)
+    if size(Odomrunfinfo,1)>0&&~isempty(Odomrunfinfo.bytes)
     if Odomrunfinfo.bytes>Odomrunbytesize
         newA=dlmread('../data/odom/odomRun.txt','',Odomrunlength,0);
         Odomrunbytesize=Odomrunfinfo.bytes;
@@ -49,20 +62,22 @@ while(1)
         Odomrunlength=Odomrunlength+size(newA,1);
         currXY=[newA(end,1);newA(end,2)];
     end
-end
+    end
      Scaninfo=dir('../data/scan/scanRun.txt');
-    if ~isempty(Scaninfo.bytes)
+    if size(Scaninfo,1)>0&&~isempty(Scaninfo.bytes)
      if Scaninfo.bytes>Scanbytesize
         newScan=dlmread('../data/scan/scanRun.txt','',Scanlength,0);
         Scanbytesize=Scaninfo.bytes;
-        plot(newScan(:,1),newScan(:,2),'r*');
+        delete(NewScanPlot);
+        plot(newScan(:,1),newScan(:,2),'k*');% old scans are black, current one is red
+        NewScanPlot=plot(newScan(:,1),newScan(:,2),'r*');
         %Scan=[Scan;newScan];
         %Scanlength=size(Scanlength,1);
         Scanlength=Scanlength+size(newScan,1);
     end
     end
     Featuresinfo=dir('../data/features/featuresRun.txt');
-    if ~isempty(Featuresinfo.bytes)
+    if size(Featuresinfo,1)>0&&~isempty(Featuresinfo.bytes)
     if Featuresinfo.bytes>Featuresbytesize
         newFeature=dlmread('../data/features/featuresRun.txt','',Featureslength,0);
         Featuresbytesize=Featuresinfo.bytes;
@@ -75,7 +90,7 @@ end
     
     
     Pxyinfo=dir('../data/cov/covRun.txt');
-    if ~isempty(Pxyinfo.bytes)
+    if  size(Pxyinfo,1)>0&&~isempty(Pxyinfo.bytes)
     if Pxyinfo.bytes>Pxybytesize
         %newA=dlmread('../data/odom/odomRun.txt','',Odomrunlength,0);
        %newA=dlmread('../data/odom/odomRun.txt','',Odomrunlength,0);
@@ -93,7 +108,20 @@ end
     end
     end
     
-    pause(.1)
+     knownFeaturesinfo=dir('../data/features/knownfeaturesRun.txt');
+    if size(knownFeaturesinfo,1)>0&& ~isempty(knownFeaturesinfo.bytes)
+    if knownFeaturesinfo.bytes>knownFeaturesbytesize
+        knownnewFeature=dlmread('../data/features/featuresRun.txt','',knownFeatureslength,0);
+        knownFeaturesbytesize=knownFeaturesinfo.bytes;
+        delete(knownFeaturesPlot);
+        knownFeaturesPlot=plot(knownnewFeature(:,1),knownnewFeature(:,2),'g*');
+        %Features=[Features;newFeature];
+        %Featureslength=size(Featureslength,1);
+        knownFeatureslength=knownFeatureslength+size(knownnewFeature,1);
+    end
+    end   
+    
+    pause(dt)
     if savevideo
     currFrame = getframe;
     writeVideo(V,currFrame)
---
+++ odometry/kalmanfilterchanged.cpp
@@ -0,0 +1,128 @@
+#include "kalmanfilter.h"
+#include <math.h>
+
+KalmanFilter::KalmanFilter(ArRobot* robot){
+  this->robot = robot;
+  
+  state = new Eigen::VectorXd(3);
+  covariance = new Eigen::MatrixXd(3,3);
+  
+  (*state) << 0.0, 0.0, 0.0;
+  (*covariance) << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+}
+
+
+void KalmanFilter::doPropagation(double dt, std::ofstream& file,std::ofstream& knownfeaturesFile) {
+  // std::cout << "\nX_hat_min =" << std::endl;
+  // std::cout << (*state) << std::endl << std::endl;
+  //std::cout << "P_min =" << std::endl;
+  //std::cout << (*covariance) << std::endl;
+  
+  
+  robot->lock();
+    double V = robot->getVel();
+    double RTV = robot->getRotVel() * 3.141592654 / 180.0;
+  robot->unlock();
+  
+  
+  int n = (*state).size();          //size of state vector, #of Landmark = (n-3)/2
+
+  double v =V/1000.0;
+  double w = RTV + 0.01; //Correct for that awesome *joke* turning that the robot can't sense
+  double sigma_v = 0.01;
+  double sigma_w = 0.04;
+  
+  Eigen::MatrixXd Q(2,2);        //Covariance of noise of Control input: linear/rotational velocity
+  
+  Eigen::MatrixXd Set;
+  
+  // Get the result of Propagation
+  Q << sigma_v, 0,
+      0, sigma_w;
+  Q = (v*v)*Q*Q;
+
+    //TIMER
+/*  std::clock_t    start;
+  start = std::clock();*/
+
+//   Set = Propagate(x_hat_min, m, 0.1, 0.01, Q, 1);
+  Set = Propagate(*state, *covariance, v, w, Q, dt);
+
+  //TIMER
+  //std::cout << "Time for propagate: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+  
+  // and parse them out to state vector and covariance matrix
+  (*state) = Set.block(0,0,n,1);
+  (*covariance) = Set.block(0,1,n,n);
+  
+  X = (*state)(0);
+  Y = (*state)(1);
+  Phi = (*state)(2);
+  
+  file << Set(0,1) << " " << Set(0,2) << " " << Set(1,1) << " " << Set(1,2) << std::endl;
+  //std::cout << Set(0,0) << " " << Set(1,1) << std::endl;
+  //std::cout << Set(0,1) << " " << Set(0,2) << " " << Set(1,1) << " " << Set(1,2) << std::endl;
+  int knownfeaturenumber= sqrt(Set.size())-3;
+  if knownfeaturenumber>0
+  {
+  	for (int a=1;a<knownfeaturenumber;a=a+1)
+  		{
+  			knownfeaturesFile << Set(3+a,0) << " "<< Set(4+a,0)<<std::endl;
+  		}
+  		
+  }
+  
+  //printf("%f\n", sin(pi/2));
+}
+
+void KalmanFilter::doUpdate(Eigen::MatrixXd z_chunk, Eigen::MatrixXd R_chunk) {
+
+  //Set Mahalanobis thresholds
+  int Gamma_max = 90;
+  int Gamma_min = 1;
+  //Variable to hold out size
+  int size;
+  // Hold out matrices state and covariance
+  Eigen::MatrixXd Set;
+
+  //TIMER
+/*  std::clock_t    start;
+  start = std::clock();*/
+
+  //Set = Update1(*state, *covariance, z_chunk, R_chunk, Gamma_max, Gamma_min);
+  //Set = Update4(*state, *covariance, z_chunk, R_chunk, Gamma_max, Gamma_min);
+  Set = Update3(*state, *covariance, z_chunk, R_chunk, Gamma_max, Gamma_min);
+
+  //TIMER
+  //std::cout << "Time for update: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+
+  
+  size = sqrt(Set.size());
+  
+  delete state;
+  delete covariance;
+  delete knownLandmarks;
+  state = new Eigen::VectorXd(size);
+  covariance = new Eigen::MatrixXd(size,size);
+  if(size > 3){
+    knownLandmarks = new Eigen::MatrixXd((size-3)/2, 2);
+  }
+  
+//   std::cout << "size is: " << size << std::endl;
+//   std::cout << "starting from index 3, going up " << size-3 << " indexes." << std::endl;
+
+  (*state) = Set.block(0, 0, size, 1);
+
+  (*covariance) = Set.block(0, 1, size, size);
+
+/*  for (int i=0; i<(size-3)/2; i++){
+
+  }
+  (*knownLandmarks) = Set.block(0,2,size-3,1);*/
+  
+  X = (*state)(0);
+  Y = (*state)(1);
+  Phi = (*state)(2);
+
+
+}
